21f112201d78f4443d5305d6c5fb28c79e73ec53
Paolo Cosentino <cosepaol88@gmail.com>
2012-05-08 14:51:13 -0500

    add weather

--- public/countdown.js
+++ public/countdown.js
@@ -92,3 +92,23 @@ $( document ).ready( function() {
             setBodyScale();
         });
 
+		
+
+			   /* Locations can be edited manually by updating 'wx_locID' below.  Please also update */
+			   /* the location name and link in the above div (wx_module) to reflect any changes made. */
+			   var wx_locID = 'MXQR0138';
+
+			   /* If you are editing locations manually and are adding multiple modules to one page, each */
+			   /* module must have a unique div id.  Please append a unique # to the div above, as well */
+			   /* as the one referenced just below.  If you use the builder to create individual modules  */
+			   /* you will not need to edit these parameters. */
+			   var wx_targetDiv = 'wx_module_9005';
+
+			   /* Please do not change the configuration value [wx_config] manually - your module */
+			   /* will no longer function if you do.  If at any time you wish to modify this */
+			   /* configuration please use the graphical configuration tool found at */
+			   /* https://registration.weather.com/ursa/wow/step2 */
+			   var wx_config='SZ=300x250*WX=FHC*LNK=SSNL*UNT=F*BGI=plane*MAP=null|null*DN=countdowntocancun.heroku.com*TIER=0*PID=1314197778*MD5=e6730643dc76f9b086ff5177b7de61b6';
+
+			   document.write('<scr'+'ipt src="'+document.location.protocol+'//wow.weather.com/weather/wow/module/'+wx_locID+'?config='+wx_config+'&proto='+document.location.protocol+'&target='+wx_targetDiv+'"></scr'+'ipt>');  
+			
